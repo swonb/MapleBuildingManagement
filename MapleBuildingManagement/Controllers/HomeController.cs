e24f6a93e8b1cc5fe9b039d508e06be8673c08db
sayda.pharma <sayda.pharma@gmail.com>
2020-02-15 23:25:15 -0500

    add link on ComplainList

---
+++ MapleBuildingManagement/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MapleBuildingManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult MainPage()
+        {
+            return View();
+        }
+
+        public ViewResult ComplainList()
+        {
+            return View();
+        }
+        public ViewResult Login()
+        {
+            return View();
+        }
+        public ViewResult ComplainFeedback()
+        {
+            return View();
+        }
+    }
+}
